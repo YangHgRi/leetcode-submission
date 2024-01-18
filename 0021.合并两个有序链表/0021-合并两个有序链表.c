c2c13bc082b6df47dd5accf169007793af219dc2
YangHgRi <1987758983@qq.com>
2024-01-19 02:30:33 +0800

    2024-01-19

--- 0021.合并两个有序链表/0021-合并两个有序链表.c
+++ 0021.合并两个有序链表/0021-合并两个有序链表.c
@@ -3,19 +3,26 @@
  * struct ListNode {
  *     int val;
  *     struct ListNode *next;
- * }
+ * };
  */
-struct ListNode *mergeTwoLists(struct ListNode *list1, struct ListNode *list2) {
-    if (list1 == NULL) {
-        return list2;
+
+struct ListNode* mergeTwoLists(struct ListNode* list1, struct ListNode* list2) {
+    struct ListNode dummy;
+    struct ListNode* current = &dummy;
+
+    while (list1 != NULL && list2 != NULL) {
+        if (list1->val < list2->val) {
+            current->next = list1;
+            list1 = list1->next;
+        } else {
+            current->next = list2;
+            list2 = list2->next;
+        }
+        current = current->next;
     }
-    if (list2 == NULL) {
-        return list1;
-    }
-    if (list1->val <= list2->val) {
-        list1->next = mergeTwoLists(list1->next, list2);
-        return list1;
-    }
-    list2->next = mergeTwoLists(list1, list2->next);
-    return list2;
+
+    // 处理剩余的节点
+    current->next = (list1 != NULL) ? list1 : list2;
+
+    return dummy.next;
 }
